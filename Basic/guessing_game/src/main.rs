1a26096958b646b2696b57e44b987c203cde70e3
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-12-24 20:59:05 +0530

    Chapter 1 io, match, expect, rand lib, cmp lib Ordering fn()

---
+++ Basic/guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main(){
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is:{}", secret_number);
+
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    // let apple = 5; -> immutable by default
+    // let mut apple = 5; -> mutable by using mut keyword
+    io::stdin()
+        .read_line(&mut guess) // return result variant either Ok or Err
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("To Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You win"),
+    }
+}
