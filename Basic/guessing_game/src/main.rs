9c17532e851b3bc88d1bc650b6790c089b6c007d
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-12-25 13:58:38 +0530

    Chapter 1 loop break continue - Done

--- Basic/guessing_game/src/main.rs
+++ Basic/guessing_game/src/main.rs
@@ -8,21 +8,32 @@ fn main(){
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is:{}", secret_number);
 
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    // let apple = 5; -> immutable by default
-    // let mut apple = 5; -> mutable by using mut keyword
-    io::stdin()
-        .read_line(&mut guess) // return result variant either Ok or Err
-        .expect("Failed to read line");
+    loop{
+    
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        // let apple = 5; -> immutable by default
+        // let mut apple = 5; -> mutable by using mut keyword
+        io::stdin()
+            .read_line(&mut guess) // return result variant either Ok or Err
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) =>  continue,
+        };
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("To Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You win"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
