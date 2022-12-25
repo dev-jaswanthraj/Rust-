ede18045d689477c941039fffe9b5d4493f8bdd6
Jaswanthraj R <jaswanthraj.r@gmail.com>
2022-12-25 14:04:00 +0530

    Chapter2 Initial Commit

--- Basic/guessing_game/src/main.rs
+++ Basic/guessing_game/src/main.rs
@@ -6,7 +6,7 @@ fn main(){
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is:{}", secret_number);
+    // println!("The secret number is:{}", secret_number);
 
     loop{
     
